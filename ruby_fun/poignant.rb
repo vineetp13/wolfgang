04b866b329cf0d324c83f9537c4b54752a590d8d
Vineet Pandey <vipandey@eng.ucsd.edu>
2015-05-04 18:46:40 -0700

    ruby: trivial first commit

---
+++ ruby_fun/poignant.rb
@@ -0,0 +1,18 @@
+
+#Poignant - 125 onwards..
+
+# Not implemented - just a list of topics
+# String interpolation using arrays and lists
+# American cash uses: :, ", 0, *, !, @
+# And more: /, , , ;, 
+
+#delimited types - scrolls to array/list
+# ex: %w{ The Winged scrolled carrier}, %x, %Q
+
+#duplicates
+#.clone vs .dup: wrt frozen objects, metaclass copy, 
+
+#
+
+
+
